845d98c82a816bd50e917933bbaced780edfb1c5
Shmaither <shmaither@gmail.com>
2022-10-01 17:03:22 -0600

    refactor: implement dataclasses and typing libraries to improve class representantion readability

--- test_code.py
+++ test_code.py
@@ -5,15 +5,17 @@ from timer import Timer
 def main():
     # t = Timer(text="Downloaded 10 tutorials in {:0.2f} seconds")
     # if constructor inside variables aren't defined it will assing it to the first it encounters inside the class.
-    t = Timer('download', logger=None)
-    for num in range(10):
-        t.start()
-        tutorial = feed.get_article(num)
-        t.stop()
-        print(tutorial)
+    # t = Timer('download', logger=None)
+    t = Timer()
 
-    download_time = Timer.timers['download']
-    print(f'Downloaded 10 tutorials in {download_time:.4f} seconds')
+    # for num in range(10):
+    t.start()
+    #     tutorial = feed.get_article(num)
+    t.stop()
+    # print(tutorial)
+
+    # download_time = Timer.timers['download']
+    # print(f'Downloaded 10 tutorials in {download_time:.4f} seconds')
 
 
 if __name__ == '__main__':
--- timer.py
+++ timer.py
@@ -1,25 +1,29 @@
-
-from asyncio.log import logger
 import time
+from dataclasses import dataclass, field
+from typing import Any, ClassVar
 
 
 class TimerError(Exception):
     """A custom exception used to report errors in use of Timer class"""
 
+# The @dataclass decorator defines Timer as a data class.
 
+
+@dataclass
 class Timer:
+    # The special ClassVar annotation is necessary for data classes to specify that .timers is a class variable.
+    timers: ClassVar = {}
+    name: Any = None
+    text: Any = 'Elapse time: {:0.4f} seconds.'
+    logger: Any = print
+    # Define special visibility for this attribute. Don't init with construct or be seeing under class representation call with type() or others.
+    _start_timer: Any = field(default=None, init=False, repr=False)
 
-    timers = {}
-
-    def __init__(self, name=None, text='Elapse time: {:0.4f} seconds.', logger=print):
-        self._start_timer = None
-        self.name = name
-        self.text = text
-        self.logger = logger
-
+    def __post_init__(self):
+        """Initialization: add timer to dict of timers"""
         # if name is passed, add new named timers to dictionary of timers.
-        if name:
-            self.timers.setdefault(name, 0)
+        if self.name:
+            self.timers.setdefault(self.name, 0)
 
     def start(self):
         if self._start_timer is not None:
